2c23c1fef1ba923e5f8e7ca736388902c9f96bcd
KCaverly <kylebcaverly@gmail.com>
2023-01-24 20:05:54 -0500

    progress on search function for scribe

--- src/scribe.rs
+++ src/scribe.rs
@@ -5,7 +5,6 @@ use std::fs::rename;
 pub struct Scribe {}
 
 impl Scribe {
-
     // Public Functions
     pub fn create(title: String, category: Option<String>, tags: Option<Vec<String>>) {
         let note = Note::new(title, category, tags);
@@ -13,7 +12,6 @@ impl Scribe {
     }
 
     pub fn transfer(path: &str, category: &str) -> std::io::Result<()> {
-
         // Transfer File Over
         let old_path = ScribePath::from(path);
         let mut new_path = ScribePath::from(path);
@@ -34,13 +32,15 @@ impl Scribe {
                 let og_data = fs::read_to_string(&res.0).unwrap();
                 let new_data = og_data.replace(&res.1, &new_path.relative_path());
 
-                let mut f = std::fs::OpenOptions::new().write(true).truncate(true).open(&res.0)?;
+                let mut f = std::fs::OpenOptions::new()
+                    .write(true)
+                    .truncate(true)
+                    .open(&res.0)?;
                 f.write_all(new_data.as_bytes())?;
                 f.flush()?;
             }
         }
         Ok(())
-
     }
 
     pub fn interactive_create() {
@@ -51,9 +51,36 @@ impl Scribe {
         unimplemented!("Interactive Transfer not yet implemented!");
     }
 
-    pub fn search(search_string: &str) -> Result<Vec<(String, String)>, Box<dyn Error>> {}
+    pub fn search(search_string: &str) -> Result<Vec<(String, String)>, Box<dyn Error>> {
+        let mut matches: Vec<(String, String)> = vec![];
+        for entry in WalkDir::new(&*NOTES_DIR).into_iter().filter_map(|e| e.ok()) {
+            if entry.path().is_file() {
+                let file_data =
+                    fs::read(entry.path().display().to_string()).expect("Cannot open file!");
+                let matcher = RegexMatcher::new(&search_string)?;
+                Searcher::new().search_slice(
+                    &matcher,
+                    &file_data,
+                    UTF8(|lnum, line| {
+                        let mymatch = matcher.find(line.as_bytes())?.unwrap();
+                        matches.push((
+                            entry.path().display().to_string(),
+                            line[mymatch].to_string(),
+                        ));
+                        Ok(true)
+                    }),
+                )?;
+            }
+        }
+        Ok(matches)
+    }
 
-    pub fn fuzzy_finder(search_options: Vec<String>, prompt: &str, multi: bool) -> Vec<Arc<dyn SkimItem>> {}
+    pub fn fuzzy_finder(
+        search_options: Vec<String>,
+        prompt: &str,
+        multi: bool,
+    ) -> Vec<Arc<dyn SkimItem>> {
+    }
 
     pub fn pull() -> bool {
         unimplemented!("Git Pull not yet implemented!");
@@ -62,6 +89,4 @@ impl Scribe {
     pub fn save(commit_message: &str) -> bool {
         unimplemented!("Git Push not yet implemented!");
     }
-    
-
 }
