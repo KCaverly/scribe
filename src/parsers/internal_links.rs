95b8ef97f9f3e0b86af5377f468889fa8e649a2d
KCaverly <kylebcaverly@gmail.com>
2023-03-01 22:30:23 -0500

    added unit test for internal_links

--- src/parsers/internal_links.rs
+++ src/parsers/internal_links.rs
@@ -20,3 +20,23 @@ impl InternalLinks {
         return Some(full_matches);
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let mut test_links: HashSet<String> = HashSet::new();
+        // TODO: This link only pulls the first match for some reason
+        // test_links.insert("projects/test/test/scribe".to_string());
+        test_links.insert("inbox/scribe/test.txt".to_string());
+
+        // Test
+        let test_data = "This is a note\n here is a [link](projects/test/test/scribe), this is a [web link though](http://www.google.com), is is another [link](inbox/scribe/test.txt)";
+        let parsed_links = InternalLinks::parse(test_data);
+        assert!(parsed_links.is_some());
+        assert_eq!(parsed_links.unwrap(), test_links);
+    }
+}
